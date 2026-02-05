3ad4eee0d2f6409f4f9723a3305eaf2e4923c796
Linztark <anasilvesttttre@gmail.com>
2026-02-05 20:11:54 +0100

    modificaciones para el detail unrouted

--- src/app/component/jugador/view-routed/jugador-view.ts
+++ src/app/component/jugador/view-routed/jugador-view.ts
@@ -1,55 +1,36 @@
 import { Component, signal, OnInit, inject } from '@angular/core';
-import { ActivatedRoute, RouterLink, Router } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { HttpErrorResponse } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { JugadorService } from '../../../service/jugador-service';
 import { IJugador } from '../../../model/jugador';
 import { serverURL } from '../../../environment/environment';
+import { JugadorDetailAdminUnrouted } from '../detail-admin-routed/jugador-detail';
 
 @Component({
   selector: 'app-jugador-view',
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, JugadorDetailAdminUnrouted],
   templateUrl: './jugador-view.html',
   styleUrls: ['./jugador-view.css'],
 })
 export class JugadorViewRouted implements OnInit {
 
   private route = inject(ActivatedRoute);
-  private jugadorService = inject(JugadorService);
-  private router = inject(Router);
+  //private snackBar = inject(MatSnackBar);
 
   jugador = signal<IJugador | null>(null);
   loading = signal(true);
   error = signal<string | null>(null);
+  id_jugador = signal<number>(0);
 
   ngOnInit(): void {
     const idParam = this.route.snapshot.paramMap.get('id');
-    const id = idParam ? Number(idParam) : NaN;
-
-    if (isNaN(id)) {
+    this.id_jugador.set(idParam ? Number(idParam) : NaN);
+    if (isNaN(this.id_jugador())) {
       this.error.set('ID no válido');
       this.loading.set(false);
       return;
     }
-
-    this.load(id);
-  }
-
-  load(id: number) {
-    this.loading.set(true);
-    this.error.set(null);
-
-    this.jugadorService.getById(id).subscribe({
-      next: (data: IJugador) => {
-        this.jugador.set(data);
-        this.loading.set(false);
-      },
-      error: (err: HttpErrorResponse) => {
-        console.error('Error al cargar el jugador:', err);
-        this.error.set('No se pudo cargar la información del jugador');
-        this.loading.set(false);
-      }
-    });
   }
 
   getImagenUrl(imagen: string | null): string {
@@ -57,7 +38,4 @@ export class JugadorViewRouted implements OnInit {
     return `${serverURL}/jugador/imagen/${imagen}`;
   }
 
-  goBack(): void {
-    this.router.navigate(['/jugador']);
-  }
 }
